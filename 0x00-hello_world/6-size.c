ef38b09873543d5bf4ea70059fea88ee5717d442
Tychool <ok.hezekiahs@gmail.com>
2022-09-08 15:25:54 -0700

    noil

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - main block
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-
-printf("size of a char: %i byte(s)\n", (unsigned long)sizeof(char));
-printf("size of an int: %i byte(s)\n", (unsigned long)sizeof(int));
-printf("size of a long int: %i byte(s)\n", (unsigned long)sizeof(long int));
-printf("size of a long long int:%i byte(s)\n", (unsigned long)sizeof(long long int));
-printf("size of a float: %i byte(s)\n", (unsigned long)sizeof(float));
+printf("Size of a char: %i byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int:%i byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
