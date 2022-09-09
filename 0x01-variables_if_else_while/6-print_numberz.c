92a7c3406b4b01c848325ffffe2302652a7f3dc5
Tychool <ok.hezekiahs@gmail.com>
2022-09-09 07:24:20 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 20;
+
+	while (i < 30)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
