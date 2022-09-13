6f615db878dd6e38960d5dee26b3bbe6345d5cba
Tychool <ok.hezekiahs@gmail.com>
2022-09-13 05:13:52 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Main is the entry point.
+ * The program prints putchar followed by a new line.
+ * And always return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of code
+ * This program prints "_putchar" followed by a new line.
+ * Return: 0
+ */
+char main(void)
+	char _,p,u,t,c,h,a,r;
+{
+	_putchar('_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n');
+	return (0);
+}
