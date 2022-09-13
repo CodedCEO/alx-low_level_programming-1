25e823886928fbabd8be4a1f4d1ab29cbc253b8b
Tychool <ok.hezekiahs@gmail.com>
2022-09-13 16:57:42 -0700

    print absolute values

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int a)
 	if (a <= 98)
 	{
 		for (a = a; a <= 97; a++)
-			printf("%d, ", n);
+			printf("%d, ", a);
 	}
 	else
 		for (a = a; a > 98; a--)
-			printf("%d, ", n);
+			printf("%d, ", a);
 	printf("98\n");
 }
