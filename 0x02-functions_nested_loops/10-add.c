434c281d71d2ba23bf63ca5bdde3a0cc65f6707d
Tychool <ok.hezekiahs@gmail.com>
2022-09-13 16:47:31 -0700

    add o and p

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two int and returns the result
+ * @o: number being added
+ * @p: number beign added
+ * Return: returns result
+ */
+int add(int o, int p)
+{
+	return (o + p);
+}
