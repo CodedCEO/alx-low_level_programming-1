9527677023075a01188bc07371ddcb7f91121e5b
Tychool <ok.hezekiahs@gmail.com>
2022-09-14 03:10:54 -0700

    create main.h header file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef HEZEKIAHS_H
+#define HEZEKIAHS_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int minth, int day, int year);
+
+#endif /* HEZEKIAHS_H */
