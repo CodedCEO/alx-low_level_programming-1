cced30c78cce6b992892db9d2cbfa72bb3233f9d
Tychool <ok.hezekiahs@gmail.com>
2022-09-15 14:04:32 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,11 @@
-#ifndef HEZEKIAHS_H
-#define HEZEKIAHS_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_remaining_days(int minth, int day, int year);
+void print_remaining_days(int month, int day, int year);
 
-#endif /* HEZEKIAHS_H */
+#endif /* HOLBERTON_H */
