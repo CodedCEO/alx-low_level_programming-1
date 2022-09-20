3a7cca56f0dc100d75c346eaea50e25ee6d6a0c7
Tychool <ok.hezekiahs@gmail.com>
2022-09-19 17:05:00 -0700

    arrays an pointers

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef HEZEKIAHS_H
+#define HEZEKIAHS_H
+
+#include <stdio.h>
+
+void positivee_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HEZEKIAHS_H */
