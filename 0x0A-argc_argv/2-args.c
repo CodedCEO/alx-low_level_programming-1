4362e92bb6c60d63d15eb46a80d99e244b423f97
Tychool <ok.hezekiahs@gmail.com>
2022-10-03 04:53:40 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print all arguments of the program separated by new line.
- * @argc: argument count or size of arguments.
- *  @argv: argument vector
- *
- *  return: 0
- */
+ * main - prints all arguments
+*main - print all arguments of the program separated by new line
+*@argc: argument count or size of arguments.
+*@argv: argument vector
+*
+* Return: 0
+*/
 int main(int argc, char **argv)
 {
 	int number = 0;
